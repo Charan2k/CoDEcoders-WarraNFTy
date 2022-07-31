d1e95bfc82eca6820d25db9055b368806cdcf1c6
Charan Vaddepally <charan316@Charans-BacMook-Air.local>
2022-08-01 03:28:31 +0530

    functionality added for local file system for a database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+var fs = require('fs');
 
 const app = express();
 app.use(cors());
@@ -19,11 +20,41 @@ app.post('/payment', (req, res) => {
         valid_till: new_date
     }
 
+    const uid = Math.ceil(Math.random()*69696969) + "nft" + Math.ceil(Math.random()*69696969);
     console.log(new_date);
+    filelPath = "./db/" + uid + ".json";
+    fs.writeFile(filelPath, JSON.stringify(dbdata), function(err) {
+        if(err) {
+            console.error("Could not write file: %s", err);
+        }
+    });
     res.send({
-        uid: Math.ceil(Math.random()*6900) + "charan",
+        uid: uid
     });
 })
+
+app.post('/verifyProduct', (req, res) => {
+    const { uid } = req.body;
+    const filelPath = "./db/" + uid + ".json";
+    fs.readFile(filelPath, function(err, data) {
+        if(err) {
+            console.error("Could not read file: %s", err);
+        }
+        const dbdata = JSON.parse(data);
+        const {id, counter, valid_till} = dbdata;
+        const date = new Date();
+        const current_date = date.toUTCString();
+        if(current_date > valid_till) {
+            res.send({
+                status: "expired"
+            });
+        } else {
+            res.send({
+                status: "valid"
+            });
+        }
+    })
+});
 
 app.get('/test', (req, res) => {
     res.send("Hello World");
