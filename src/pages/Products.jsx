ab1a43f0830125f0f718f8db7c0e10b11b8eaa15
Charan Vaddepally <charan316@Charans-BacMook-Air.local>
2022-07-31 20:14:41 +0530

    changed method and diff product ids

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -9,13 +9,13 @@ export default function Products() {
         </div>
         <div   className="row">
             <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1234} />
-            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1234} />
-            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1234} />
-            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1234} />
-            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1234} />
-            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1234} />
-            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1234} />
-            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1234} />
+            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1235} />
+            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1236} />
+            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1237} />
+            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1238} />
+            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1239} />
+            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1241} />
+            <Productcard img="https://i.ibb.co/b7ZVzYr/product-2.jpg" name="Red Printed T-shirt" price="500.00" id={1242} />
           <Pagination/>
         </div>
       </div>
